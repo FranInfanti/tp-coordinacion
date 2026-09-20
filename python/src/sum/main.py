defc5dbff06de8cd6ba007ef3644181fc86bbdee
FranInfanti <francisco.infanti@gmail.com>
2026-09-20 21:43:33 +0000

    refactor(sum): refactor code

--- python/src/sum/main.py
+++ python/src/sum/main.py
@@ -44,6 +44,22 @@ class SumFilter:
         self.fruit_by_req = {}
         self.fruit_by_req_lock = threading.Lock()
 
+    def _publish_data(self, req_id, amount_by_fruit):
+        logging.info(f"Broadcast data message for req_id={req_id}")
+
+        for final_fruit_item in amount_by_fruit.values():
+            for data_output_exchange in self.data_output_exchanges:
+                data_output_exchange.send(
+                    message_protocol.internal.serialize(
+                        [req_id, final_fruit_item.fruit, final_fruit_item.amount]
+                    )
+                )
+
+    def _publish_eof(self, req_id, exchanges):
+        logging.info(f"Broadcast EOF message for req_id={req_id}")
+        for exchange in exchanges:
+            exchange.send(message_protocol.internal.serialize([req_id]))
+ 
     def _process_data(self, req_id, fruit, amount):
         with self.fruit_by_req_lock:
             logging.info(f"Process data for req_id={req_id}")
@@ -59,27 +75,16 @@ class SumFilter:
         logging.info(f"Process EOF for req_id={req_id}")
 
         with self.fruit_by_req_lock:
-            amount_by_fruit = self.fruit_by_req_lock.get(req_id)
+            amount_by_fruit = self.fruit_by_req.get(req_id)
             if not amount_by_fruit:
                 return
 
-            logging.info(f"Broadcast data message for req_id={req_id}")
-            for final_fruit_item in amount_by_fruit.values():
-                for data_output_exchange in self.data_output_exchanges:
-                    data_output_exchange.send(
-                        message_protocol.internal.serialize(
-                            [req_id, final_fruit_item.fruit, final_fruit_item.amount]
-                        )
-                    )
-
-            logging.info(f"Broadcast EOF message for req_id={req_id}")
-            for data_output_exchange in self.data_output_exchanges:
-                data_output_exchange.send(message_protocol.internal.serialize([req_id]))
+            self._publish_data(req_id, amount_by_fruit)
+            self._publish_eof(req_id, self.data_output_exchanges)
 
             del self.fruit_by_req[req_id]
 
-        for eof_exchange in self.eof_exchanges:
-            eof_exchange.send(message_protocol.internal.serialize([req_id]))
+        self._publish_eof(req_id, self.eof_exchanges)
 
     def process_data_messsage(self, message, ack, nack):
         fields = message_protocol.internal.deserialize(message)
